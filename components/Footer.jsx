763c22238028b5ae226ec9cd2cedfacddb29a872
vedant-11 <69393820+vedant-11@users.noreply.github.com>
2022-02-16 19:54:17 +0530

    updated page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Footer = () => {
   return (
@@ -8,17 +8,22 @@ const Footer = () => {
           Interested?
         </h1>
         <p className="my-8">Join our waitist to be updated</p>
-        <input
-          type="email"
-          placeholder="please enter your email"
-          className=" sm:border-r-2 poppins font-medium rounded-r-none sm:rounded-r border-r-0 sm:w-80 h-10 rounded text-[#868E96] px-4 border-2 border-[#495057] w-96 bg-[#212529]"
-        />
-        <button
-          type="submit"
-          className=" font-medium sm:rounded-l sm:mt-8 poppins rounded rounded-l-none w-40 bg-[#F8F9FA] text-[#192024] h-10"
-        >
-          Join the waitlist
-        </button>
+        <form>
+          <input
+            type="email"
+            onChange={(e) => {
+              SetEmail(e.target.value);
+            }}
+            placeholder="please enter your email"
+            className=" sm:border-r-2 poppins font-medium rounded-r-none sm:rounded-r border-r-0 sm:w-80 h-10 rounded text-[#868E96] px-4 border-2 border-[#495057] w-96 bg-[#212529]"
+          />
+          <button
+            type="submit"
+            className=" font-medium sm:rounded-l sm:mt-8 poppins rounded rounded-l-none w-40 bg-[#F8F9FA] text-[#192024] h-10"
+          >
+            Join the waitlist
+          </button>
+        </form>
       </div>
       <div className="flex overflow-hidden h-40 bg-[#161B22] justify-between items-center sm:flex-col sm:justify-evenly sm:items-start ">
         <span className="text-3xl font-bold  overflow-hidden bg-[#161B22] mx-24 sm:mx-5 sm:text-3xl poppins font-semibold ">
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import Navbar from "../components/Navabar";
+import React from "react";
 import back from "../assets/bg.png";
 import HowItWorks from "../components/HowItWorks";
 import Image from "next/image";
+
 import Footer from "../components/Footer";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef();
+  const finalRef = React.useRef();
+
   return (
     <>
       <Navbar />
@@ -18,9 +41,36 @@ export default function Home() {
           <p className=" poppins font-normal  my-10 text-xl bg-transparent sm:text-lg sm:text-center  ">
             connecting artists with their fans like never before.
           </p>
-          <button className="poppins font-medium rounded bg-[#F8F9FA] text-[#192024] w-40 h-10 hover:bg-[#C9C9C9] ">
+          <button
+            onClick={onOpen}
+            className="poppins font-medium rounded bg-[#F8F9FA] text-[#192024] w-40 h-10 hover:bg-[#C9C9C9] "
+          >
             Enter the future
           </button>
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Join the Waitlist!</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>First name</FormLabel>
+                  <Input ref={initialRef} placeholder="Enter your email" />
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <button className=" hover:bg-[#C9C9C9] poppins font-regular bg-[#F8F9FA] text-[#192024] w-40 h-10 rounded-md ">
+                  Join the waitlist
+                </button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </div>
       </div>
       <div className=" h-screen flex flex-row items-center justify-around mx-24 sm:flex-col sm:mx-5 sm:justify-evenly">
@@ -36,7 +86,7 @@ export default function Home() {
           <p className="text-center mb-16 poppins font-medium text-2xl ">
             fans
           </p>
-          <h1 className=" font-semibold herofont text-5xl overflow-hidden text-center">
+          <h1 className=" h-28 font-semibold herofont text-5xl overflow-hidden text-center">
             Own your favorite song
           </h1>
         </div>
